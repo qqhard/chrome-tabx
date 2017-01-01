730e23286d1efc3863c1759f20db7df8263e826d
hard <453824417@qq.com>
2017-01-01 23:01:56 +0800

    do not remove

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 var IS_START = 'isstart';
 var IS_REMOVE = 'isremove';
+var IS_MOVE = 'ismove';
 var FALLOFF = 'falloff';
 var THD_REMOVE = 'thdremove';
 var ROUND = 'round';
 var INIT_WEIGHT = 'initweight';
 var ADD_WEIGHT = 'addweight';
+var MAX_TAB = 'maxtab';
 var TYPE_GET = 'GET';
 var TYPE_SET = 'SET';
 var TYPE_ADD = 'ADD';
@@ -20,8 +22,9 @@ var closeUrl = [];
 var param = {
     isstart: true,
     isremove: true,
+    ismove: false,
+    maxtab: 15,
     falloff: 0.9,
-    thdremove: 0.1,
     round: 10,
     initweight: 2,
     addweight: 2
@@ -29,26 +32,27 @@ var param = {
 
 function getUrlWeight(url) {
     var info = urlInfo[url];
-    if(!!info){
+    if (!!info) {
         return info.weight;
     }
     return 0;
 }
 
-function setUrlWeight(url,weight) {
+function setUrlWeight(url, weight) {
     var info = urlInfo[url];
-    if(!!info) info.weight = weight;
-    else urlInfo[url] = {weight:weight};
+    if (!!info) info.weight = weight;
+    else urlInfo[url] = {weight: weight};
 }
 
 function initParams() {
     function initParam(name) {
-        chrome.storage.local.get(name,function (value) {
-            if(!!value[name]){
+        chrome.storage.local.get(name, function (value) {
+            if (!!value[name]) {
                 param[name] = value[name];
             }
         });
     }
+
     initParam(IS_START);
     initParam(IS_REMOVE);
     initParam(FALLOFF);
@@ -61,38 +65,35 @@ function initParams() {
 
 function getDomain(url) {
     var re = /.*\/\/(.*?)\//;
-    return re.exec(url+"/")[1];
+    return re.exec(url + "/")[1];
 }
 
 function getUrl(url) {
     var re = /(.*?)#/;
-    return re.exec(url+"#")[1];
+    return re.exec(url + "#")[1];
 }
 
-function getTitle(url) {
-    
-}
 function isBlankUrl(url) {
-    if(!url || url.indexOf("chrome://")>=0) return true;
+    if (!url || url.indexOf("chrome://") >= 0) return true;
     return false;
 }
 
 function addWeight(url) {
     url = getUrl(url);
     var info = urlInfo[url];
-    if(!!info && !!info.weight) setUrlWeight(url, info.weight + param[ADD_WEIGHT]);
+    if (!!info && !!info.weight) setUrlWeight(url, info.weight + param[ADD_WEIGHT]);
     else setUrlWeight(url, param[INIT_WEIGHT]);
-    console.log(url+' change weight to '+urlInfo[url].weight);
+    console.log(url + ' change weight to ' + urlInfo[url].weight);
 }
 
-function cmpstr(a,b) {
+function cmpstr(a, b) {
     var lena = a.length;
     var lenb = b.length;
-    if(lena != lenb)return lena - lenb;
-    else{
-        for(var i=0;i<lena;i++){
-            if(a[i]<b[i])return -1;
-            else if(a[i]>b[i])return 1;
+    if (lena != lenb)return lena - lenb;
+    else {
+        for (var i = 0; i < lena; i++) {
+            if (a[i] < b[i])return -1;
+            else if (a[i] > b[i])return 1;
         }
     }
 }
@@ -100,23 +101,15 @@ function cmpstr(a,b) {
 function adjustTabs(tabs) {
     var arrs = [];
     var domainScore = [];
-    var tabsForDel = [];
-    var tabNum = tabs.length;
-    for(var i in tabs){
+    for (var i in tabs) {
         var tab = tabs[i];
-        if(isBlankUrl(tab.url))continue;
+        if (isBlankUrl(tab.url))continue;
         var weight = getUrlWeight(tab.url);
-        if(weight == MAYBE_INIT){
-            setUrlWeight(tab.url,param[INIT_WEIGHT]);
+        if (weight == MAYBE_INIT) {
+            setUrlWeight(tab.url, param[INIT_WEIGHT]);
             continue;
         }
-        if(weight == PRE_TO_DELETE && tabNum > 7){
-            tabsForDel.push(tab.id);
-            closeUrl.push(tab.url);
-            delete urlInfo[tab.url];
-            tabNum--;
-            continue;
-        }
+
         var node = {
             oldIndex: tab.index,
             tabId: tab.id,
@@ -124,96 +117,104 @@ function adjustTabs(tabs) {
             domain: getDomain(tab.url),
             weight: getUrlWeight(tab.url),
         };
-        if(!domainScore[node.domain]) domainScore[node.domain] = {count:1,weight:node.weight};
-        else{
+
+        if (!domainScore[node.domain]) domainScore[node.domain] = {count: 1, weight: node.weight};
+        else {
             var ds = domainScore[node.domain];
             ds.weight += node.weight;
             ds.count += 1;
         }
         arrs.push(node);
     }
-    if(param[IS_REMOVE]){
-        chrome.tabs.remove(tabsForDel);
-    }
 
-    for(var i in arrs){
+    for (var i in arrs) {
         var ds = domainScore[arrs[i].domain];
         arrs[i].domainScore = ds.weight / ds.count;
     }
-    arrs.sort(function (a,b) {
-        if(a.domain == b.domain){
-            if(Math.abs(a.weight - b.weight) < 1e-6 )return a.oldIndex - b.oldIndex;
+    arrs.sort(function (a, b) {
+        if (a.domain == b.domain) {
+            if (Math.abs(a.weight - b.weight) < 1e-6)return a.oldIndex - b.oldIndex;
             return b.weight - a.weight;
         }
-        if(Math.abs(a.domainScore - b.domainScore) < 1e-6)return cmpstr(a.domain,b.domain);
+        if (Math.abs(a.domainScore - b.domainScore) < 1e-6)return cmpstr(a.domain, b.domain);
         return b.domainScore - a.domainScore;
-    })
-    for(var i = 0;i < arrs.length;i++){
-        var tab = arrs[i];
-        if(tab.oldIndex != i){
-            chrome.tabs.move(tab.tabId, {
-                index: i
-            });
+    });
+
+    if (param[IS_REMOVE]) {
+        var tabsForDel = [];
+        for (var i = param[MAX_TAB]; i < arrs.length; i++) {
+            tabsForDel.push(arrs[i].tabId);
+        }
+        chrome.tabs.remove(tabsForDel);
+    }
+
+    if (param[IS_MOVE]) {
+        for (var i = 0; i < arrs.length; i++) {
+            var tab = arrs[i];
+            if (tab.oldIndex != i) {
+                chrome.tabs.move(tab.tabId, {
+                    index: i
+                });
+            }
         }
     }
 }
 
 
-
 function mainLoop() {
-    if(!param[IS_START]){
-        setTimeout(mainLoop,param[ROUND]*1000);
-        return ;
+    if (!param[IS_START]) {
+        setTimeout(mainLoop, param[ROUND] * 1000);
+        return;
     }
-    for(var url in urlInfo){
+    for (var url in urlInfo) {
         var weight = getUrlWeight(url);
-        if(weight < param[THD_REMOVE]){
-            if(weight == PRE_TO_DELETE){
+        if (weight < param[THD_REMOVE]) {
+            if (weight == PRE_TO_DELETE) {
 
 
-            }else{
+            } else {
                 setUrlWeight(url, PRE_TO_DELETE);
             }
-        }else {
-            setUrlWeight(url,weight * param[FALLOFF]);
+        } else {
+            setUrlWeight(url, weight * param[FALLOFF]);
         }
     }
 
-    for(var tabId in tabToUrl){
+    for (var tabId in tabToUrl) {
         var url = tabToUrl[tabId];
         var weight = getUrlWeight(url);
-        if(weight < param[THD_REMOVE]){
+        if (weight < param[THD_REMOVE]) {
             delete tabToUrl[tabId];
         }
     }
 
 
     setTimeout(function () {
-        chrome.windows.getCurrent({populate:true},function (window) {
+        chrome.windows.getCurrent({populate: true}, function (window) {
             adjustTabs(window.tabs);
             console.log("###")
             // console.log(window.tabs);
             console.log(urlInfo);
             console.log("###")
         });
-    },1000);
+    }, 1000);
 
-    setTimeout(mainLoop,param[ROUND]*1000);
+    setTimeout(mainLoop, param[ROUND] * 1000);
 }
 
 function updateTab(tabId, url) {
-    if(!!url){
+    if (!!url) {
         url = getUrl(url);
         var oldUrl = tabToUrl[tabId];
         var urlWeight = getUrlWeight(url);
         var oldWeight = getUrlWeight(oldUrl);
-        if(!!urlWeight) {
-            setUrlWeight(url,Math.max(param[INIT_WEIGHT],urlWeight));
-        }else{
-            if(!!oldWeight && getDomain(url) == getDomain(oldUrl)){
-                setUrlWeight(url,Math.max(param[INIT_WEIGHT], oldWeight));
-            }else{
-                setUrlWeight(url,param[INIT_WEIGHT]);
+        if (!!urlWeight) {
+            setUrlWeight(url, Math.max(param[INIT_WEIGHT], urlWeight));
+        } else {
+            if (!!oldWeight && getDomain(url) == getDomain(oldUrl)) {
+                setUrlWeight(url, Math.max(param[INIT_WEIGHT], oldWeight));
+            } else {
+                setUrlWeight(url, param[INIT_WEIGHT]);
             }
         }
         tabToUrl[tabId] = url;
@@ -222,28 +223,27 @@ function updateTab(tabId, url) {
 
 
 function startListeners() {
-    chrome.tabs.onCreated.addListener(function(tab){
+    chrome.tabs.onCreated.addListener(function (tab) {
         console.log(tab);
     });
 
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-        console.log('Tab '+tabId+' has been changed with these options:');
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        console.log('Tab ' + tabId + ' has been changed with these options:');
         console.log(changeInfo);
         var url = changeInfo.url;
         updateTab(tabId, url);
     });
 
-    chrome.tabs.onMoved.addListener(function(tabId, moveInfo){
+    chrome.tabs.onMoved.addListener(function (tabId, moveInfo) {
         //console.log(moveInfo);
     });
 
-    chrome.tabs.onActivated.addListener(function(activeInfo){
-        console.log('Tab '+activeInfo.tabId+' in window '+activeInfo.windowId+' is active now.');
+    chrome.tabs.onActivated.addListener(function (activeInfo) {
+        console.log('Tab ' + activeInfo.tabId + ' in window ' + activeInfo.windowId + ' is active now.');
     });
 
-    chrome.runtime.onMessage.addListener(function (message,sender,sendResponse) {
-        switch(message.type)
-        {
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+        switch (message.type) {
             case TYPE_ADD:
                 addWeight(sender.tab.url);
                 break;
--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 var IS_START = 'isstart';
 var IS_REMOVE = 'isremove';
+var IS_MOVE = 'ismove';
 var FALLOFF = 'falloff';
 var ROUND = 'round';
+var MAX_TAB = 'maxtab'
 var THD_REMOVE = 'thdremove';
 var INIT_WEIGHT = 'initweight';
 var ADD_WEIGHT = 'addweight';
@@ -11,8 +13,8 @@ var PARAM = 'param';
 
 function disableInputs(disabled) {
     var inputs = document.getElementsByTagName('input');
-    for(var i = 0;i < inputs.length;i++){
-        if(inputs[i].getAttribute('name') != IS_START){
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].getAttribute('name') != IS_START) {
             inputs[i].disabled = disabled;
         }
     }
@@ -24,14 +26,15 @@ function queryAndRender() {
         type: TYPE_GET,
         name: PARAM
     };
-    chrome.runtime.sendMessage(message,function (param) {
-        if(!param[IS_START]){
+    chrome.runtime.sendMessage(message, function (param) {
+        if (!param[IS_START]) {
             disableInputs(true);
         }
         document.getElementById(IS_START).checked = param[IS_START];
         document.getElementById(IS_REMOVE).checked = param[IS_REMOVE];
-        document.getElementById(FALLOFF).value = parseInt(param[FALLOFF]*100);
-        document.getElementById(THD_REMOVE).value = parseInt(param[THD_REMOVE]*10);
+        document.getElementById(IS_MOVE).checked = param[IS_MOVE];
+        document.getElementById(MAX_TAB).value = parseInt(param[MAX_TAB]);
+        document.getElementById(FALLOFF).value = parseInt(param[FALLOFF] * 100);
         document.getElementById(ROUND).value = param[ROUND];
         document.getElementById(INIT_WEIGHT).value = param[INIT_WEIGHT];
         document.getElementById(ADD_WEIGHT).value = param[ADD_WEIGHT];
@@ -40,15 +43,15 @@ function queryAndRender() {
 
 queryAndRender();
 
-function changeParam(name,value,fun) {
+function changeParam(name, value, fun) {
     var message = {
         type: TYPE_SET,
         name: name,
         value: value
     };
-    if(!!fun){
-        chrome.runtime.sendMessage(message,fun);
-    }else{
+    if (!!fun) {
+        chrome.runtime.sendMessage(message, fun);
+    } else {
         chrome.runtime.sendMessage(message);
     }
 
@@ -56,48 +59,54 @@ function changeParam(name,value,fun) {
 
 document.getElementById(IS_START).onchange = function (event) {
     var value = event.target.checked;
-    changeParam(IS_START,value,function () {
+    changeParam(IS_START, value, function () {
         disableInputs(!value);
     });
 }
 
 document.getElementById(IS_REMOVE).onchange = function (event) {
     var value = event.target.checked;
-    changeParam(IS_REMOVE,value);
+    changeParam(IS_REMOVE, value);
+}
+
+document.getElementById(IS_MOVE).onchange = function (event) {
+    var value = event.target.checked;
+    changeParam(IS_MOVE, value);
+}
+
+document.getElementById(MAX_TAB).onchange = function (event) {
+    var value = event.target.value;
+    if (!!value) {
+        changeParam(MAX_TAB, parseInt(value));
+    }
 }
 
 document.getElementById(FALLOFF).onchange = function (event) {
     var value = event.target.value;
-    if(!!value){
-        changeParam(FALLOFF,parseFloat(value)/100.0);
+    if (!!value) {
+        changeParam(FALLOFF, parseFloat(value) / 100.0);
     }
 }
 
 document.getElementById(ROUND).onchange = function (event) {
     var value = event.target.value;
-    if(!!value){
-        changeParam(ROUND,parseInt(value));
+    if (!!value) {
+        changeParam(ROUND, parseInt(value));
     }
 }
 
-document.getElementById(THD_REMOVE).onchange = function (event) {
-    var value = event.target.value;
-    if(!!value){
-        changeParam(THD_REMOVE,parseFloat(value)/10.0);
-    }
-}
 
 document.getElementById(INIT_WEIGHT).onchange = function (event) {
     var value = event.target.value;
-    if(!!value){
-        changeParam(INIT_WEIGHT,parseInt(value));
+    if (!!value) {
+        changeParam(INIT_WEIGHT, parseInt(value));
     }
 }
 
 document.getElementById(ADD_WEIGHT).onchange = function (event) {
     var value = event.target.value;
-    if(!!value){
-        changeParam(ADD_WEIGHT,parseInt(value));
+    if (!!value) {
+        changeParam(ADD_WEIGHT, parseInt(value));
     }
 }
 
